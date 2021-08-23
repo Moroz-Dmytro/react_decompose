174d8a59d3642bcb03f37d97447d845808d7fae6
Moroz Dmytro <dmytro101100@gmail.com>
2021-08-23 16:02:39 +0300

    Updates

---
+++ precache-manifest.a24a3f70a7d2a5a64c992a841c7364ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6d57c07ffb9caa87febe3f67161fbe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6b2b39b932ab65aa079",
+    "url": "./static/css/main.0a9c4c00.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b2b39b932ab65aa079",
+    "url": "./static/js/main.ca415fc8.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
